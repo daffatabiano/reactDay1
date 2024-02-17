4904956bb03e0cdf581a18bb976cca84bb5cdb9d
daffatabiano <149911161+daffatabiano@users.noreply.github.com>
2024-02-17 14:14:46 +0700

    react_3
    react 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
-import {useState} from "react";
-import Sidebar from './components/Sidebar'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const App = () => {
-  const [showSidebar, setShowSidebar] = useState(false);
 
-  const handleSidebar = () => {
-    setShowSidebar (!showSidebar)
+function App() {
+  // const [users, setUsers] = useState([])
+  const [menus, setMenus] = useState([]);
+
+  // const getUserData = () => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/users')
+  //     .then(res => setUsers(res.data))
+  //     .catch(err => console.log(err));
+  // }
+
+  const getMenu = () => {
+    axios
+    .get('https://api.mudoapi.tech/menus')
+    .then(res => setMenus(res.data.data.Data))
+    .catch(err => {
+      console.log(err);
+    })
+
   }
 
+  useEffect(() => {
+    getMenu();
+  },[])
+
+  console.log('data',menus);
+
+  // useEffect(() => { 
+  //   getUserData();
+  //  }, []);
+
+  //  console.log('data', users);
+
   return (
-    <div>
+    <>
+      <h1>hello world</h1>
 
-    <button onClick={handleSidebar}>|||</button>
-      {/* <button onClick ={handleSidebar}>|||</button>
-      {showSidebar ? <div>
-      <p>Home</p>
-      <p>About</p>
-      <p>Contact</p>
-    </div> : null}
-     */}
+      {menus.map((items, index) => (
+        <div key={index}>
+          <h1>{items.name}</h1>
+          <img style={{width: '200px'}} src={items.imageUrl} />
+          <p>{items.description}</p>
+        </div>
+      ))}
 
-     {showSidebar && (
-      <Sidebar/>
-      )}
-    </div>
+      {/* {menus.map((item, index) => (
+        <div key={index}>
+          <h1>{item.name}</h1>
+          <img src={item.imageUrl} />
+          <p>{item.description}</p>
+        </div>
+      ))} */}
+    </>
   )
 }
 
